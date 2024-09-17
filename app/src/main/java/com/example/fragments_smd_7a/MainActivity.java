d07c587c3ebf32773e688e9140610e60d08dea56
Rana Waqas Waqas <programmingwithranawaqas@gmail.com>
2024-09-17 20:27:46 +0500

    Initial commit

---
+++ app/src/main/java/com/example/fragments_smd_7a/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.fragments_smd_7a;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.constraintlayout.widget.ConstraintSet;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    View portrait, lanscape;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        portrait = findViewById(R.id.portrait);
+
+        if(portrait!=null)
+        {
+            Toast.makeText(this, "Phone is in Portrait mode", Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            Toast.makeText(this, "Phone is in lanscape mode", Toast.LENGTH_SHORT).show();
+        }
+
+
+
+    }
+}
